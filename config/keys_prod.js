567c59cfb80935065bba31685d394af3c06d8d32
Samson Wayne <smuhadia4@gmail.com>
2024-07-18 11:07:18 +0000

    added configuration instructions for the authentication routes

---
+++ config/keys_prod.js
@@ -0,0 +1,10 @@
+module.exports = {
+  mongoURI: process.env.MONGO_URI,
+  secretOrKey: process.env.SECRET_OR_KEY,
+  googleClientID: process.env.GOOGLE_CLIENTID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  facebookClientId: process.env.FACEBOOK_CLIENTID,
+  cookieKey: 'cookieKey',
+  emailConfirm: process.env.EMAIL_CONFIRM,
+  passwordConfirm: process.env.PASSWORD_CONFIRM
+};
