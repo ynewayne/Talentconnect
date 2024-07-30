ad2d4cfd287cd8090d7e2ae2fdc4c5ad78c3c0b5
Samson Wayne <smuhadia4@gmail.com>
2024-07-30 09:00:44 +0000

    adjusted the models to suit the new db

--- models/UserG.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-
-// Create Schema
-const UserSchema = new Schema({
-  googleId: String
-});
-
-module.exports = UserG = mongoose.model('usersg', UserSchema);
