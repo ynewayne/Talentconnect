eb6808f495b752cc9bff418664a3f0d21e549b5e
Samson Wayne Muhadia <smuhadia4@gmail.com>
2024-07-30 11:55:03 +0300

    Updated passport.js

--- routes/services/passport.js
+++ routes/services/passport.js
@@ -1,68 +1,59 @@
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-var keys = require('../../config/keys');
-var passport = require('passport');
+const keys = require('../../config/keys');
+const passport = require('passport');
 const mongoose = require('mongoose');
-const User = mongoose.model('usersg');
-var jwt = require('jsonwebtoken');
-var Users = require('../../models/User');
+const User = require('../../models/User'); // Make sure this path is correct
+const jwt = require('jsonwebtoken');
 
-
+// Serialize user
 passport.serializeUser((user, done) => {
-done(null, user.id);
+  done(null, user.id);
 });
 
+// Deserialize user
 passport.deserializeUser((id, done) => {
-User.findById(id)
-.then(user => {
-done(null, user);
-});
+  User.findById(id)
+    .then(user => {
+      done(null, user);
+    })
+    .catch(err => done(err, null));
 });
 
-Google authentication
+// Google authentication
 passport.use(new GoogleStrategy({
-clientID: keys.googleClientID,
-clientSecret: keys.googleClientSecret,
-callbackURL: '/api/authgoogle/callback',
-callbackURL: '/dashboard',
-proxy: true
-},
-(accessToken, refreshToken, profile, done) => {
- let data = profile._json;
-console.log(data);
-console.log(data.displayName);
-console.log(data.emails[0].value);
-console.log(data.image.url);
-User.findOne({ googleId: profile.id })
-.then((existingUser) => {
-if(existingUser){
-// We have a user record
- done(null, existingUser);
-} else {
-      // This is a new user
-new User({googleId: profile.id})
-.save()
-.then(user => done(null, user));
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/api/authgoogle/callback',
+    proxy: true
+  },
+  (accessToken, refreshToken, profile, done) => {
+    let data = profile._json;
+    User.findOne({ googleId: profile.id })
+      .then((existingUser) => {
+        if (existingUser) {
+          // We have a user record
+          return done(null, existingUser);
+        } else {
+          // This is a new user
+          const email = data.emails[0].value;
+          const name = data.displayName;
+          const avatar = data.picture;
 
-var email = data.emails[0].value;
-var name = data.displayName;
-var avatar = data.image.url;
+          const newUser = new User({
+            googleId: profile.id,
+            name: name,
+            email: email,
+            avatar: avatar,
+            password: profile.id  // Note: it's better to handle password more securely
+          });
 
-var newUser = new Users({
-name: name,
-email: email,
-avatar: avatar,
-password: profile.id
-})
+          newUser.save()
+            .then(user => done(null, user))
+            .catch(err => done(err, null));
+        }
+      })
+      .catch(err => done(err, null));
+  }
+));
 
-newUser
-.save()
-.then(user => res.json(user))
-.catch(err => console.log(err))
-}
-// login page logic
-errors.email = 'User not found';
-return res.status(404).json(errors);
-
-})
-})
-);
+module.exports = passport;
